798bd1dcc323cff84c422a6ae64ed1815f28a647
Erige <erige.benraies@supcom.tn>
2025-01-26 18:21:40 +0100

    first commit

---
+++ PayGauge/frontend/src/utils/API_LINKS.js
@@ -0,0 +1,8 @@
+export default {
+    // USER: "https://paygauge.vercel.app/api/user",
+    USER: "http://192.168.56.1:8000/api/user",
+
+    // TRANSACTION: "https://paygauge.vercel.app/api/transaction"
+    TRANSACTION: "http://localhost:8000/api/transaction"
+
+}
